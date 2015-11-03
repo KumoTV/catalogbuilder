2bb4e1152d6e51c1daaad204b8296aec9f3a782c
Fernando <fcgravalos@gmail.com>
2015-11-03 16:12:51 -0500

    RSpec fix

--- spec/features/media_items/create_media_item_spec.rb
+++ spec/features/media_items/create_media_item_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.feature "Media Items management", :type => :feature do
   scenario "User creates a new media item" do
     def create_media_item(options = {})
+      options = {}
       ts = Time.now.strftime('%Y%m%d%H%M%S')
       options[:name] ||= "TEST_MEDIA_ITEM_#{ts}"
       options[:origin_s3_url] ||= "s3://test_bucket/assets/raw/test_#{ts}.mp4"
@@ -10,10 +11,10 @@ RSpec.feature "Media Items management", :type => :feature do
       options[:encoding_profile] ||= "HLS_DEFAULT_FULL_HD"
       visit "/media_items/new"
       expect(page).to have_text("New Media Item")
-      fill_in "Media item name", :with => "MY_MEDIA_ITEM_#{ts}"
-      fill_in "Origin s3 url", :with => "s3://test_bucket/assets/raw/mymediaitem.ts"
-      fill_in "Destination s3 url", :with => "s3://test_bucket/assets/"
-      fill_in "Encoding profile", :with => "HLS_DEFAULT_FULL_HD"
+      fill_in "Media item name", :with => options[:name]
+      fill_in "Origin s3 url", :with => options[:origin_s3_url]
+      fill_in "Destination s3 url", :with => options[:destination_s3_url]
+      fill_in "Encoding profile", :with => options[:encoding_profile]
       click_button "Create Media item"
       expect(page).to have_text("Media item was successfully created.")
     end
