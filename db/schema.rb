ff4589bb1abb294dbe6e24d395a0747b88e49f5b
Fernando <fcgravalos@gmail.com>
2015-11-04 18:22:26 -0500

    Media Item name should be unique and validation tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102220830) do
+ActiveRecord::Schema.define(version: 20151105000330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,5 +24,7 @@ ActiveRecord::Schema.define(version: 20151102220830) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
   end
+
+  add_index "media_items", ["media_item_name"], name: "index_media_items_on_media_item_name", unique: true, using: :btree
 
 end
