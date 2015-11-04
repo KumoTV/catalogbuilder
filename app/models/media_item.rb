97df94e077c12c31ce8610f0e82ec350af3336b4
Fernando <fcgravalos@gmail.com>
2015-11-03 19:01:35 -0500

    First tests for media items

--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -1,2 +1,7 @@
 class MediaItem < ActiveRecord::Base
+  validates_presence_of :media_item_name, :origin_s3_url, :destination_s3_url
+  validates :origin_s3_url, format: { with: /s3:\/\/(([^\/]+)\/(.*?([^\/]+)\.(ts|avi|mp4|mov)))/,
+                                      message: "That is not valid source s3 url for a raw asset."}
+  validates :destination_s3_url, format: { with: /s3:\/\/(([^\/]+)\/(.*?([^\/]+))\/?)/,
+                                           message: "That is not a valid destination s3 url for an encoded asset."}
 end
--- spec/features/media_items/create_media_item_spec.rb
+++ spec/features/media_items/create_media_item_spec.rb
@@ -1,13 +1,11 @@
 require "rails_helper"
 
 RSpec.feature "Media Items management", :type => :feature do
-  scenario "User creates a new media item" do
     def create_media_item(options = {})
-      options = {}
       ts = Time.now.strftime('%Y%m%d%H%M%S')
       options[:name] ||= "TEST_MEDIA_ITEM_#{ts}"
       options[:origin_s3_url] ||= "s3://test_bucket/assets/raw/test_#{ts}.mp4"
-      options[:destination_s3_url] ||= "TEST_MEDIA_ITEM_#{ts}"
+      options[:destination_s3_url] ||= "s3://test_bucket/assets/transcoded/#{ts}"
       options[:encoding_profile] ||= "HLS_DEFAULT_FULL_HD"
       visit "/media_items/new"
       expect(page).to have_text("New Media Item")
@@ -16,7 +14,27 @@ RSpec.feature "Media Items management", :type => :feature do
       fill_in "Destination s3 url", :with => options[:destination_s3_url]
       fill_in "Encoding profile", :with => options[:encoding_profile]
       click_button "Create Media item"
-      expect(page).to have_text("Media item was successfully created.")
     end
+  
+  scenario "User creates a new media item" do
+    create_media_item
+    expect(page).to have_text("Media item was successfully created.")
+  end
+  
+  scenario "Redirects to media item index on success" do
+    create_media_item
+    expect(page).to have_text("Media item was successfully created.")
+    click_link "Back"
+    expect(page).to have_text("Listing Media Items")
+  end
+  
+  scenario "Fails if origin s3 url is not valid" do
+    create_media_item({:origin_s3_url => "s3://test/test2/novalid"})
+    expect(page).to have_text("Origin s3 url That is not valid source s3 url for a raw asset.")
+  end
+  
+  scenario "Fails if destination s3 url is not valid" do
+    create_media_item({:destination_s3_url => "s4://test/test2/novalid/"})
+    expect(page).to have_text("Destination s3 url That is not a valid destination s3 url for an encoded asset.")
   end
 end
