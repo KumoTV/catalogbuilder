cf452840b809c0b376dcacb472fd916314f2d5f0
Fernando <fcgravalos@gmail.com>
2015-11-10 17:23:45 -0500

    Sns Connection Pool and subscription confirmation test.

--- config/initializers/sns_pool.rb
+++ config/initializers/sns_pool.rb
@@ -7,7 +7,7 @@ aws_region = ENV["aws_region"]
 sns_connection_pool_size= ENV["sns_connection_pool_size"]
 sns_max_pending_notifications= ENV["sns_max_pending_notifications"]
 
-require 'modules/aws_service.rb'
+require 'aws_service'
 include AwsService
 ::SnsConnectionPool = AwsService::SnsConnectionPool.new({:access_key_id => aws_access_key_id, 
                                               :secret_access_key => aws_secret_access_key}, 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :media_items
+  post 'sns_endpoint' => 'sns#sns_endpoint'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
